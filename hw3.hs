3f248f8662f60356253ccf567173ad72446862f5
hzach <zftidwell@gmail.com>
2016-05-16 22:53:20 -0500

    initial commit

---
+++ hw3.hs
@@ -0,0 +1,52 @@
+
+{- Below are the excerises from Real Worlf Haskell Chapter 3-}
+
+{- 3.a: Recursive list type -}
+
+-- Custom List type
+data List a = Cons a (List a)
+              | Nil
+              deriving Show
+
+-- Converts a Haskell list to the custom List
+toList :: [a] -> List a
+toList (x:xs) = Cons x (toList xs)
+toList []     = Nil
+
+
+-- Converse of toList
+fromList :: List a -> [a]
+fromList (Cons x (xs)) = x:(fromList xs)
+fromList Nil           = []
+
+
+{- 3.b: Single parameter Tree type with Maybe value -}
+data Maybe a = Just a | Nothing
+data  Tree a = Node (Tree a) (Tree a) | Maybe a
+
+
+{- 3.1, 3.2 -}
+length' :: [a] -> Int
+length' []     = 0
+length' (_:xs) = 1 + length' xs
+
+
+{- 3.3 -}
+mean :: (Fractional a) => [a] -> a
+mean xs = (1/n) * sum' xs
+          where
+            n = fromIntegral $ length xs
+            sum' (y:ys) = y + (sum' ys)
+            sum' []     = 0
+
+{- 3.4 -}
+palindrome :: [a] -> [a]
+palindrome xs = xs ++ reverse' xs
+                where
+                  reverse' []     = []
+                  reverse' (x:xs) = (reverse' xs) ++ [x]
+
+{- 3.5 -}
+intersperse :: a -> [[a]] -> [a]
+intersperse _ []      = []
+intersperse c (x:xs)  = x ++ c:(intersperse c xs)
